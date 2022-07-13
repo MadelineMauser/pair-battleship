98b42c511f851a93cade67cdedfb6440314ce3af
Alex M <Alexm717@hotmail.com>
2022-07-13 15:35:28 -0700

    working method vaild placement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe(Board) do
     expect(@board.cells.length).to(eq(16))
     expect(@board.cells["A1"]).to(be_a(Cell))
   end
+
+  it("can test for vaild coordinate") do
+    expect(@board.valid_coordinate?("A1")).to(eq(true))
+    expect(@board.valid_coordinate?("D4")).to(eq(true))
+    expect(@board.valid_coordinate?("A5")).to(eq(false))
+    expect(@board.valid_coordinate?("E1")).to(eq(false))
+    expect(@board.valid_coordinate?("A22")).to(eq(false))
+  end
+
+  it("text") do
+  end
 end
