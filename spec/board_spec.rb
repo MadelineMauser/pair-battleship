219a681ce0761c7506a28bec391ad264f65e78c8
Alex M <Alexm717@hotmail.com>
2022-07-14 10:15:10 -0700

    test for valid placement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -36,8 +36,24 @@ RSpec.describe(Board) do
 
   it("can make sure coordinates are consecutive") do
     @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
     expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to(eq(false))
-    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to(eq(true))
-    expect(@board.valid_placement?(@cruiser, ["A1", "B1", "C1"])).to(eq(true))
+    expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to(eq(false))
+    expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to(eq(false))
+    expect(@board.valid_placement?(@cruiser, ["C1", "B1"])).to(eq(false))
+  end
+
+  it("coordinates cant be diagonal ") do
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C4"])).to(eq(false))
+    expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to(eq(false))
+  end
+
+  it("should have valid placement") do
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to(eq(true))
+    expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to(eq(true))
   end
 end
