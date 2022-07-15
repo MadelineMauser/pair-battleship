fd57514701e79874b80eee2fc456fa86553838f0
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-15 16:31:48 -0600

    add method for generating coordinates

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,14 @@
 class Game
 
+  def initialize
+    @player_board = Board.new
+    @computer_board = Board.new
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_sub = Ship.new("Submarine", 2)
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_sub = Ship.new("Submarine", 2)
+  end
+
   def welcome
     puts "Welcome to BATTLESHIP \nEnter p to play. Enter q to quit"
     input = gets.chomp.downcase
@@ -13,6 +22,17 @@ class Game
   end
 
   def play
+    puts "#{generate_coordinates(@computer_sub)}"
+  end
 
+  def generate_coordinates(ship)
+    generated_coordinates = []
+    until generated_coordinates != []
+      working_coordinates = @computer_board.cells.keys.sample(ship.length)
+      if @computer_board.valid_placement?(ship, working_coordinates)
+        generated_coordinates = working_coordinates
+      end
+    end
+    return generated_coordinates
   end
 end
