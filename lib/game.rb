9166ef7c73b2cc5559cb159ddb2377af2e145601
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-18 18:10:39 -0600

    Add end game

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,7 @@ class Game
     @computer_sub = Ship.new("Submarine", 2)
     @player_cruiser = Ship.new("Cruiser", 3)
     @player_sub = Ship.new("Submarine", 2)
+    @is_playing = false
   end
 
   def welcome
@@ -22,13 +23,15 @@ class Game
   end
 
   def play
+    @is_playing = true
     ## Place Computer ships on board
     @computer_board.place(@computer_cruiser, generate_coordinates(@computer_cruiser, @computer_board))
     @computer_board.place(@computer_sub, generate_coordinates(@computer_sub, @computer_board))
     player_setup
-    until (@computer_cruiser.sunk? && @computer_sub.sunk?) || (@player_cruiser.sunk? && @player_sub.sunk?)
-    turn
+    while @is_playing == true do
+      turn
     end
+  end_game
   end
 
     ##
@@ -84,22 +87,64 @@ class Game
     @player_board.render(true)
     puts "Enter the coordinate for your shot:"
     inputted_coordinate = gets.chomp.upcase
-    until @computer_board.cells[inputted_coordinate].fired_upon? == false
-      puts "You've already fired on this coordinate. \nPlease choose a new coordinate:"
-      inputted_coordinate = gets.chomp.upcase
-    end
+
     until @player_board.valid_coordinate?(inputted_coordinate)
       puts "Please enter a valid coordinate:"
       inputted_coordinate = gets.chomp.upcase
     end
+
+    until @computer_board.cells[inputted_coordinate].fired_upon? == false
+      puts "You've already fired on this coordinate. \nPlease choose a new coordinate:"
+      inputted_coordinate = gets.chomp.upcase
+    end
+
     @computer_board.cells[inputted_coordinate].fire_upon
+
     working_cell_mark = @computer_board.cells[inputted_coordinate].render
     if working_cell_mark == "H"
       puts "Your shot on #{inputted_coordinate} was a hit!"
     elsif working_cell_mark == "X"
-      puts "Your shot on #{inputted_coordinate} was a hit! Your opponent's ship has sunk!"
+      puts "Your shot on #{inputted_coordinate} was a hit! My ship has sunk!"
     else
       puts "Your shot on #{inputted_coordinate} was a miss."
+    end
+  end_game_check
+    computer_coordinate = nil
+    until computer_coordinate != nil
+      working_coordinate = @computer_board.cells.keys.sample
+
+      if @player_board.cells[working_coordinate].fired_upon? == false
+        computer_coordinate = working_coordinate
+      end
+    end
+    @player_board.cells[computer_coordinate].fire_upon
+    computer_cell_mark = @player_board.cells[computer_coordinate].render
+    if computer_cell_mark == "H"
+      puts "My shot on #{computer_coordinate} was a hit!"
+    elsif computer_cell_mark == "X"
+      puts "My shot on #{computer_coordinate} was a hit! Your ship has sunk!"
+    else
+      puts "My shot on #{computer_coordinate} was a miss."
+    end
+    if (@computer_cruiser.sunk? && @computer_sub.sunk?) || (@player_cruiser.sunk? && @player_sub.sunk?)
+    end
+  end
+
+  def end_game_check
+    if (@computer_cruiser.sunk? && @computer_sub.sunk?) || (@player_cruiser.sunk? && @player_sub.sunk?)
+      @is_playing = false
+    end
+  end
+
+  def end_game
+    puts "COMPUTER BOARD".center(40, "=")
+    @computer_board.render(true)
+    puts "PLAYER BOARD".center(40, "=")
+    @player_board.render(true)
+    if @computer_cruiser.sunk? && @computer_sub.sunk?
+      puts "You won!"
+    else
+      puts "I won!"
     end
   end
 end
